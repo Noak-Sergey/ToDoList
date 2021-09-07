6ad325b668b5f97dd705112542ec9953b63b80df
Noak-Sergey <noakstuff@gmail.com>
2021-09-07 19:27:56 +0300

    13.5 commit

---
+++ src/api/todolist-api.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: `https://social-network.samuraijs.com/api/1.1/`,
+    withCredentials: true,
+    headers: {
+        'API-KEY': '35086c75-b096-4bcf-9631-0257dc2ea2c7'
+    }
+})
+
+type TodoType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+
+type CommonResponseType<T = {}> = {
+    fieldsErrors: Array<string>
+    messages: Array<string>
+    resultCode: number
+    data: T
+}
+
+export const todolistApi = {
+    getTodolist() {
+        return instance.get<Array<TodoType>>('todo-lists')
+    },
+    createTodo(title: string) {
+        return instance.post<CommonResponseType<{item: TodoType}>>('todo-lists', {title})
+    },
+    deleteTodo(todolistId: string) {
+        return instance.delete<CommonResponseType>(`todo-lists/${todolistId}`)
+    },
+    updateTodo(todolistId: string, title: string) {
+        return instance.put<CommonResponseType>(`todo-lists/${todolistId}`, {title})
+    }
+}
---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react'
+import {todolistApi} from "../api/todolist-api";
+
+export default {
+    title: 'API'
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todolistApi.getTodolist()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const title = ''
+        todolistApi.createTodo(title)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        todolistApi.deleteTodo(todolistId)
+            .then ((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        const title = ''
+        todolistApi.updateTodo(todolistId, title)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
